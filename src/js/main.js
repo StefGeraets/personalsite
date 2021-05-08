c88e0a5b4bc55ff24579e675d2b210398efb47fe
Stef Geraets <stef@getnoticed.nl>
2021-05-08 19:42:43 +0200

    Hero section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
-import * as theme from './lib/theme.js'
+// import * as theme from './lib/theme.js'
 
 document.documentElement.classList.remove('no-js')
+
+const toolTipElement = document.querySelector('.tooltip')
+
+toolTipElement.addEventListener('touchstart touchend', function(e) {
+  e.preventDefault();
+  this.classList.toggle('hover');
+})
